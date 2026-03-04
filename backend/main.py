29dc08c90fd878295156cccbba77ea190825497f
Jaspersen21 <ludiguj@gmail.com>
2026-03-04 21:34:07 +0300

    Fixed mongodb connection and bcrypt version issue

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from database import database
+from schemas import UserCreate
+from auth import hash_password 
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "KaziConnect Backend Running 🚀"}
+
+@app.post("/register")
+async def register(user: UserCreate):
+
+    #Check if users already exxits 
+    existing_user = await database.users.find_one({"email": user.email})
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    #Hash password 
+    hashed_pw = hash_password(user.password)
+
+    #create user document 
+    user_dict = user.dict()
+    user_dict["pasword"] = hashed_pw
+
+    #Insert user into database 
+    await database.users.insert_one(user_dict)
+
+    return {"message": "User registered successfully"}
