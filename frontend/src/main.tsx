4909fc685f4bfea1f9721560625a4f194c73c7fb
Jaspersen21 <ludiguj@gmail.com>
2026-06-05 14:46:50 +0300

    Fix deployment import paths

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import { AuthProvider } from './context/AuthContext.tsx'
-import { queryClient } from './lib/query-client.ts'
+import { AuthProvider } from './context/AuthContext'
+import { queryClient } from './lib/query-client'
 import { QueryClientProvider } from '@tanstack/react-query'
 
 createRoot(document.getElementById('root')!).render(
